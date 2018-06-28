580de94b2d700dac5059da79788f4aef8c91fb99
MMandirola <marcelo.mandirola@correo.ucu.edu.uy>
2018-06-28 19:59:50 -0300

    updateGrammar

--- Obligatorio/Babble.hs
+++ Obligatorio/Babble.hs
@@ -1,7 +1,8 @@
 import qualified Data.Map as Map
 import System.Random
+import Data.List
 data Symbol =NoTerm String | Term String
-    deriving(Show,Eq)
+    deriving(Show,Eq, Ord)
 type Prod = (String , [([Symbol],Double)])
 type BabbleGrammar = (String, [Prod])
 
@@ -39,7 +40,34 @@ generateValidStrings x y z = do
     return (generateRandomStrings x y z (randomRs (0.0,1.0) gen))
 
 generateRandomStrings:: BabbleGrammar-> Int -> (Int,Int,[Prod]) -> [Double] ->[String]
-generateRandomStrings _ _ _ _= ["holi"]
+generateRandomStrings (initial, productions) , n, (min, max, []) (r:rs) =
+    | n == 0 = initial
+    | 
+
+    where 
+        prods = (Map.fromList productions) Map.! initial
+        probs = accumulatedProbability (map \x (snd x) prods)
+        index = elemIndex (take 1 (filter(\x -> x > r) probs))
+        elem = fst(productions !! index)
+
+generateRandomStrings g@(intial, productions)  n  (min, max, newProductions)  rnn = generateRandomStrings (normalizeGrammar (updateGrammar g newProductions)) n (min, max, []) rnn 
+
+accumulatedProbability:: [Double] -> [Double]
+accumulatedProbability [] = []
+accumulatedProbability l@(x:xs) = [1 - sum(xs)] ++  accumulatedProbability(xs)
+
+updateGrammar ::BabbleGrammar -> [Prod] -> BabbleGrammar
+updateGrammar grammar [] = grammar
+updateGrammar grammar ((nt, []):xs) = updateGrammar grammar xs
+updateGrammar grammar@(initial, productions) ((nt, symbols):xs)
+        |Map.member nt prods = updateGrammar ( initial, (Map.toList (Map.insert nt (map 
+            (\x -> if (Map.member (fst x) sym) then (fst x, sym Map.! (fst x)) else x)
+            (prods Map.! nt)) prods))) xs
+        |otherwise = updateGrammar grammar xs 
+        where 
+            prods = Map.fromList productions
+            sym = Map.fromList symbols
+
 
 normalizeGrammar::BabbleGrammar->BabbleGrammar
 normalizeGrammar  (a,[])=  (a,[])
@@ -50,6 +78,6 @@ normalizeProd ts = [(a,(b/total)) | (a,b)<- ts]
     where total = foldl (\z y -> z +( snd y)) 0 ts
 
 test1::BabbleGrammar
-test1 = ("Exp" ,[( "Exp",[([(NoTerm "Exp"),(Term "+"),(NoTerm "Exp")],0.9),
+test1 = ("Exp" ,[( "Exp",[([(NoTerm "Exp"),(NoTerm "Exp")],0.9),
     ([(NoTerm "Exp"),(Term "+"),(NoTerm "Exp")],0.91)])])
 
